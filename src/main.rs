3aa4b67313c325df46b99be8fc2a29d6d876ebf1
Haddock <averagetryhardbear@gmail.com>
2022-09-29 09:55:28 +0200

    Add rot13 test

--- src/main.rs
+++ src/main.rs
@@ -22,14 +22,18 @@ where
     T: Write,
 {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        // collect bytes from buf with alphabet ascii characters rotated by 13 places
         let rotated_buf: Vec<u8> = buf
             .iter()
             .map(|byte| {
+                // get beginning of alphabet, either upper or lowercase, or return early
                 let offset = match *byte {
                     b'A'..=b'Z' => b'A',
                     b'a'..=b'z' => b'a',
+                    // not in alphabet, return as is
                     _ => return *byte,
                 };
+                // rotate byte by 13 places
                 return offset + (*byte - offset + 13) % 26;
             })
             .collect();
@@ -54,9 +58,23 @@ fn main() {
     );
 }
 
+#[cfg(test)]
 mod tests {
+    use crate::*;
+
+
     #[test]
+    // Shamelessly reusing the exercise here
     fn test_rot13() {
-        todo!()
+        let mut content = Vec::<u8>::default();
+
+        let mut buff = Rot13Writer::new(&mut content);
+        buff.write(b"Lbh penpxrq zl fhcre qvssvphyg pbqvat punyyratr... pbqr vf ddommNst")
+            .unwrap();
+
+        assert_eq!(
+            content.iter().map(|x| *x as char).collect::<String>(),
+            "You cracked my super difficult coding challenge... code is qqbzzAfg".to_string()
+        );
     }
 }
